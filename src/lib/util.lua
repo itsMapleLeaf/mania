d52dfeb68ee803412b167bf8b9bec7e03829c14c
just nobody <kingdaro@gmail.com>
2017-03-30 20:24:24 -0500

    added more util functions

--- src/lib/util.lua
+++ src/lib/util.lua
@@ -8,4 +8,22 @@ function util.split(str, delimiter)
   return result
 end
 
+function util.map(items, mapper)
+  local result = {}
+  for i = 1, #items do
+    result[i] = mapper(items[i], i)
+  end
+  return result
+end
+
+function util.filter(items, predicate)
+  local result = {}
+  for i, item in ipairs(items) do
+    if predicate(item, i) then
+      table.insert(result, item)
+    end
+  end
+  return result
+end
+
 return util
