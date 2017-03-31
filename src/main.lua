724e61256ee14386bf7c9d69eaf1a478479070b4
just nobody <kingdaro@gmail.com>
2017-03-30 20:36:26 -0500

    loading and displaying available maps

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,12 @@
 io.stdout:setvbuf('no')
 
-local gameplay = require 'states.gameplay'
+-- local gameplay = require 'states.gameplay'
 local osu = require 'lib.osu'
 local util = require 'lib.util'
 
-local state
+-- local state
+
+local maps = {}
 
 function love.load()
   if not love.filesystem.isDirectory('maps') then
@@ -18,17 +20,31 @@ function love.load()
   local oszFiles = util.filter(love.filesystem.getDirectoryItems('maps'), isOSZ)
   util.map(oszFiles, osu.extractOSZ)
 
-  local map
+  for _, file in ipairs(love.filesystem.getDirectoryItems('maps')) do
+    local path = 'maps/' .. file
+    if love.filesystem.isDirectory(path) then
+      for _, mapFile in ipairs(love.filesystem.getDirectoryItems(path)) do
+        local mapFilePath = path .. '/' .. mapFile
+        if love.filesystem.isFile(mapFilePath) and mapFile:match('%.osu$') then
+          table.insert(maps, mapFilePath)
+        end
+      end
+    end
+  end
 
   -- state = gameplay(map)
 end
 
-function love.update(dt)
-  -- state:update(dt)
-end
+-- function love.update(dt)
+--   -- state:update(dt)
+-- end
 
 function love.draw()
-  -- state:draw()
+  for i, map in ipairs(maps) do
+    local x = 10
+    local y = 10 + (i - 1) * 30
+    love.graphics.print(map, x, y)
+  end
 end
 
 function love.keypressed(key)
